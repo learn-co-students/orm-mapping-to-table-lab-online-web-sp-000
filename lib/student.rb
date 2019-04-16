a165c9418735244c6a3365e07f5a39c4801a4c01
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-16 01:14:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,31 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade)
+    self.name = name
+    self.grade = grade
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?,?);
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT id FROM students WHERE name = ?;", self.name)[0][0]
+  end
+
+  def self.create(hash)
+    song = Student.new(hash[:name], hash[:grade])
+    song.save
+    song
+  end
+
   def self.create_table
     sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students(
@@ -15,5 +40,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students;")
+  end
 end

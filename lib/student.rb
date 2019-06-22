25fb17861a88f398a35b9c3c235cc0fa2f5140a3
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-22 19:11:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,20 @@
 class Student
-
+  attr_reader :id, :name, :grade  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  def initialize(name, grade)
+    @name = name 
+    @grade = grade
+  end
   
+  def self.create_table
+    sql = <<- SQL 
+          CREATE TABLE IF NOT EXISTS songs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            album TEXT 
+            )
+        SQL
+        DB[:conn].execute(sql)
+  end 
 end

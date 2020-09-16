fc6de58bd596cd017d383340ddf861fb44fbc631
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-09-16 12:52:02 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -43,4 +43,33 @@ class Student
     student
   end
 
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.name = row[0]
+    new_student.grade = row[1]
+    new_student
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def find_by_name(name)
+    sql <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+
+  end
 end
